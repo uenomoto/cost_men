e2969640ca007f68179c790e0b422f108a9f8cc2
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-09 01:30:59 +0900

    health_check ok

--- backend/app/controllers/api/v1/health_check_controller.rb
+++ backend/app/controllers/api/v1/health_check_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class HealthCheckController < ApplicationController
       def index
-        render json: { message: 'ヘルパーチェックOK!!' }, status: :ok
+        render json: { message: 'ヘルスチェックOK!!' }, status: :ok
       end
     end
   end
