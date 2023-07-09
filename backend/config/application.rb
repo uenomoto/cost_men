bb2cfafc3b30b7d42728d62e08c75b5f9d025a9a
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-09 09:25:43 +0900

    rspec install

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -37,5 +37,16 @@ module CostMen
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+      g.factory_bot false
+    end
+
+    config.time_zone = 'Asia/Tokyo'
   end
 end
