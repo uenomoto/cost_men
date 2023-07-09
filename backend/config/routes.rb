043304b984c1ab26d2aac102eea8fc1ec3227d3e
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-09 09:11:16 +0900

    rubocopお通し！

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
-  namespace :api , defaults: { format: :json } do
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
     end
