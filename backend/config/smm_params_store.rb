533679f45e6e3eeaaf1fd284d5c98de61a201165
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-10 23:25:29 +0900

    自動デプロイテスト

---
+++ backend/config/smm_params_store.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'aws-sdk-ssm'
+
+ssm = Aws::SSM::Client.new(region: 'ap-northeast-1')
+
+# master_key取得
+resp = ssm.get_parameter(name: '/myapp/database/key', with_decryption: true)
+ENV['RAILS_MASTER_KEY'] = resp.parameter.value
+
+# 　DB情報取得
+%w[host username password name].each do |key|
+  resp = ssm.get_parameter(name: "/myapp/database/#{key}", with_decryption: true)
+  ENV["DATABASE_#{key.upcase}"] = resp.parameter.value
+end
+
+# これでデバックできる
+puts "Retrieved key: #{ENV['RAILS_MASTER_KEY']}"
+puts "DB:#{ENV['DATABASE_HOST']}"
+puts "DB:#{ENV['DATABASE_NAME']}"
+puts "DB:#{ENV['DATABASE_PASSWORD']}"
+puts "DB:#{ENV['DATABASE_USERNAME']}"
