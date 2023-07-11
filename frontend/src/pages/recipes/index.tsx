be9205f49cd9c221bb4c3c27d626b009a5c176a5
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-12 03:36:50 +0900

    selectBox作成

---
+++ frontend/src/pages/recipes/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { SelectBox } from "../../../components/atoms/selectbox/SelectBox";
+
+const RecipesIndex = () => {
+  return <SelectBox />;
+};
+
+export default RecipesIndex;
