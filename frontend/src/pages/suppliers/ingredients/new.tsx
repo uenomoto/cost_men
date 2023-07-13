50469123b16751ebe30fcefb9b392b38822790c6
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-13 16:41:33 +0900

    サイドバー調整 アクティブページのサイドバーリンクを色つけしました

---
+++ frontend/src/pages/suppliers/ingredients/new.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SupplierIngredientNew = () => {
+  return (
+    <h1 className="text-xl lg:text-3xl">
+      こちらのタブをクリックし仕入れ先か原材料の登録を初めてください
+    </h1>
+  );
+};
+
+export default SupplierIngredientNew;
