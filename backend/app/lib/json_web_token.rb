6fa1af55be3ab67d7e432f91520bb51f13c70708
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-17 15:15:47 +0900

    JWTクラスと認証用クラスを作成

---
+++ backend/app/lib/json_web_token.rb
@@ -0,0 +1,32 @@
+require 'net/http'
+require 'uri'
+
+class JsonWebToken
+  def self.verify(token)
+    JWT.decode(token, nil,
+      true, # このトークンの署名を検証する
+      algorithm: 'RS256',
+      iss: 'https://dev-k4qwtrhp.jp.auth0.com/', # トークンの発行者を検証する
+      verify_iss: true,
+      sud: Rails.application.secrets.auth0_api_audience,
+      verify_aud: true) do |header|
+        jwt_hash[header['kid']]
+      end
+  end
+
+  def self.jwt_hash
+    jwks_raw = Net::HTTP.get URI("https://dev-k4qwtrhp.jp.auth0.com/.well-known/jwks.json")
+    jwks_keys = Array(JSON.parse(jwts_raw)['keys'])
+    Hash[
+      jwks_keys
+      .map do |k|
+        [
+          k['kid'],
+          OpenSSL::X509::Certificate.new(
+            Base64.decode64(k['x5c'].first)
+          ).public_key
+        ]
+      end
+    ]
+  end
+end
---
+++ backend/app/services/authorization_service.rb
@@ -0,0 +1,23 @@
+class AuthorizationService
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  # トークンを検証し、ユーザーを返す
+  def current_user
+    @auth_payload, @auth_header = verify_token
+    @user = User.from_token_payload(@auth_payload)
+  end
+
+  private
+
+  # Authorizationヘッダーからトークンを取り出す
+  def http_token
+    @headers['Authorization'].split(' ').last if @headers['Authorization'].present?
+  end
+
+  # トークンを検証する
+  def verify_token
+    JsonWebToken.verify(http_token)
+  end
+end
