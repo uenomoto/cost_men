bd965d939b21fa7f402e140cd0a66400239afe41
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-17 23:45:40 +0900

    JWT取得はでき投稿できるけどリロードするとJWTが消えてしまう

--- frontend/src/pages/blog/index.tsx
+++ frontend/src/pages/blog/index.tsx
@@ -1,6 +1,6 @@
 // このページは削除する
 import { NextPage } from "next";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 // recoil
 import { useRecoilValue } from "recoil";
@@ -8,21 +8,77 @@ import { tokenState } from "../../recoil/atoms/tokenState";
 
 // 遷移したblogページでRecoilからTokneを取得できるか確認
 const BlogPage: NextPage = () => {
+  // 投稿や削除や認証がないとできないことや遷移できないページは全て↓記述すること
   const token = useRecoilValue(tokenState); // RecoilのTokneを取得する
 
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const res = await axios.get("http://192.168.2.108:3000/api/v1/posts");
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getPosts();
-  });
+  // console.log(token);
 
-  return <div>ブログ投稿</div>;
+  const [title, setTitle] = useState<string>("");
+  const [caption, setCaption] = useState<string>("");
+
+  const onClick = () => {
+    const parems = {
+      title: title,
+      caption: caption,
+    };
+    console.log(token);
+
+    axios
+      .post("http://192.168.2.108:3000/api/v1/posts", parems, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    setState: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setState(e.target.value);
+  };
+
+  // useEffect(() => {
+  //   const getPosts = async () => {
+  //     try {
+  //       const res = await axios.get("http://192.168.2.108:3000/api/v1/posts");
+  //       console.log(res.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   getPosts();
+  // });
+
+  return (
+    <div>
+      <label htmlFor="">タイトル</label>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => {
+          onChange(e, setTitle);
+        }}
+      />
+      <br />
+      <label htmlFor="">本文</label>
+      <input
+        type="text"
+        value={caption}
+        onChange={(e) => {
+          onChange(e, setCaption);
+        }}
+      />
+      <br />
+      <button onClick={onClick}>新規投稿</button>
+    </div>
+  );
 };
 
 export default BlogPage;
