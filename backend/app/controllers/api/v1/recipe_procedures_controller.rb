ac27948fbf9f70300cb9c8f159eac6dfdb862766
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-19 01:07:20 +0900

    レシピ手順忘れてました

---
+++ backend/app/controllers/api/v1/recipe_procedures_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class RecipeProceduresController < ApplicationController
+      def index; end
+
+      def create; end
+
+      def update; end
+
+      def destroy; end
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       resources :recipes, only: %i[index show create update destroy] do
         resources :recipe_ingredients, only: %i[create update], path: 'ingredients'
         resources :recipe_tags, only: %i[index create destroy], path: 'tags'
+        resources :recipe_procedures, only: %i[index create update destroy], path: 'procedures'
         resources :selling_prices, only: %i[create update]
       end
       resources :ingredients, only: %i[index show create update destroy]
