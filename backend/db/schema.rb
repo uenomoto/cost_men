de0d9fda1a09457d11de3068b5d2abedaaf2f374
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-18 20:44:52 +0900

    recipes table作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_111439) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_114052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_111439) do
     t.index ["user_id"], name: "index_auth_tokens_on_user_id"
   end
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "user_id", null: false
+    t.string "name", null: false
+    t.decimal "total_cost", precision: 8, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
+
   create_table "users", primary_key: "sub", id: :string, force: :cascade do |t|
     t.string "name", null: false
     t.text "picture"
@@ -32,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_111439) do
   end
 
   add_foreign_key "auth_tokens", "users", primary_key: "sub"
+  add_foreign_key "recipes", "users", primary_key: "sub"
 end
