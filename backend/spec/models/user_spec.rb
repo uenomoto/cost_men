25872b1ccda5f755abd952cf668d120900ec2db0
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-20 10:02:36 +0900

    試しにシンプルなUserモデルの検証テスト

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe User do
     it { is_expected.to have_many(:suppliers).dependent(:destroy) }
     it { is_expected.to have_many(:auth_tokens).dependent(:destroy) }
   end
+
+  describe 'validations' do
+    context 'subが空欄禁止であり一意であること' do
+      # FactoryBotにUserのsubを一意にするためのsequenceを定義すること
+      subject { build(:user, sub: '一意なsub') } # 保存するテストではないためbuild使用
+      it { is_expected.to validate_presence_of(:sub) }
+      it { is_expected.to validate_uniqueness_of(:sub) }
+    end
+  end
 end
