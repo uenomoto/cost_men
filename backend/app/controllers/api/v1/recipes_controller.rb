21d4b0650b17901d66a240d93be6fcfb6f3a8547
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-22 23:03:47 +0900

    Form objectsを使用して複数のモデルを1つのフォームで登録できる様になりました

--- backend/app/controllers/api/v1/recipes_controller.rb
+++ backend/app/controllers/api/v1/recipes_controller.rb
@@ -2,16 +2,34 @@
 
 module Api
   module V1
-    class RecipesController < ApplicationController
+    class RecipesController < SecuredController
+      before_action :authorize_request
+
       def index; end
 
       def show; end
 
-      def create; end
+      # レシピ登録でtagとingredientも一緒に登録する、Form Objectで作成したクラスを使用する
+      def create
+        form = RecipeTagIngredientForm.new(recipe_params)
+
+        if form.save
+          render json: { status: 'SUCCESS', data: form.recipe }, status: :created
+        else
+          render json: { status: 'ERROR', data: form.errors }, status: :unprocessable_entity
+        end
+      end
 
       def update; end
 
       def destroy; end
+
+      private
+
+      # RecipeTagIngredientFormで定義したパラメータを受け取る
+      def recipe_params
+        params.require(:recipe).permit(:recipe_name, :recipe_image, checked_tags: [], recipes: %i[id quantity])
+      end
     end
   end
 end
---
+++ backend/app/forms/recipe_tag_ingredient_form.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+# Form Objctを使用し、レシピとタグと材料を同時に登録するクラスです
+class RecipeTagIngredientForm
+  include ActiveModel::Model
+
+  # Next.js側のフォームから送られてくるデータを受け取る
+  attr_accessor :recipe_name, :recipe_image, :checked_tags, :recipes
+
+  # 読み取り専用でrecipeをcreateアクションに返す
+  attr_reader :recipe
+
+  validates :recipe_name, presence: true
+  validates :checked_tags, presence: true
+  validates :recipes, presence: true
+  validate :ingredients_must_exist, :tags_must_exist
+
+  def save
+    return false unless valid?
+
+    # トランザクションを使用し、レシピとタグと材料を同時に登録する
+    ActiveRecord::Base.transaction do
+      create_recipe
+      create_recipe_tags
+      create_recipe_ingredients
+    end
+
+    true
+  rescue ActiveRecord::RecordInvalid => e
+    errors.add(:base, e.message)
+    false
+  end
+
+  private
+
+  # method分割　レシピを登録する
+  def create_recipe
+    @recipe = Recipe.create!(name: recipe_name, image: recipe_image)
+  end
+
+  # method分割　recipeに紐づくtagを登録する(配列)
+  def create_recipe_tags
+    checked_tags.each do |tag_id, checked|
+      RecipeTag.create!(recipe: @recipe, tag_id:) if checked # checkedがtrueの場合のみ登録する
+    end
+  end
+
+  # method分割　recipeに紐づくingredientと数量を登録する(配列)
+  def create_recipe_ingredients
+    recipes.each do |ingredient|
+      # ingredient[:id]は材料のid、ingredient[:quantity]は材料の量
+      RecipeIngredient.create!(recipe: @recipe, ingredient_id: ingredient[:id], quantity: ingredient[:quantity])
+    end
+  end
+
+  # 材料のバリデーションをチェックする
+  def ingredients_must_exist
+    @ingredients = recipes.map do |ingredient|
+      Ingredient.find_by(id: ingredient[:id])
+    end.compact
+
+    return unless @ingredients.size != recipes.size
+
+    errors.add(:recipes, '一部の材料が存在しません')
+  end
+
+  def tags_must_exist
+    # 同様にタグのバリデーションもチェックする
+    @tags = checked_tags.map do |tag_id, _checked|
+      Tag.find_by(id: tag_id)
+    end.compact
+
+    return unless @tags.size != checked_tags.size
+
+    errors.add(:tags, '一部のタグが存在しません')
+  end
+end
