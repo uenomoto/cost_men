180060539fd97c6ea81b1f8173984bbae4137ab3
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-23 16:40:09 +0900

    tagtableのnameカラムからtag_nameを取得しrecipeに関するtagをpostmanで表示できました

--- backend/app/controllers/api/v1/recipe_tags_controller.rb
+++ backend/app/controllers/api/v1/recipe_tags_controller.rb
@@ -4,8 +4,11 @@ module Api
   module V1
     class RecipeTagsController < ApplicationController
       def index
-        recipe_tags = RecipeTag.where(recipe_id: params[:recipe_id])
-        render json: { recipe_tags: recipe_tags }, status: :ok
+        recipe_tags = RecipeTag.where(recipe_id: params[:recipe_id]).includes(:tag)
+        recipe_tags_with_names = recipe_tags.map do |recipe_tag|
+          recipe_tag.attributes.merge({ tag_name: recipe_tag.tag.name })
+        end
+        render json: { recipe_tags: recipe_tags_with_names }, status: :ok
       end
 
       # Form Objectで作成したので、レシピ登録時のタグ登録は不要だが、個々でタグを追加する場合は、このアクションを使う
