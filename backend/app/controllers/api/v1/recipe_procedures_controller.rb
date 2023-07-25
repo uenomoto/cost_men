592b87662f8e97a5b3a1831aa5444d6ddf1b3276
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-07-25 19:14:12 +0900

    レシピ手順Controller作成

--- backend/app/controllers/api/v1/recipe_procedures_controller.rb
+++ backend/app/controllers/api/v1/recipe_procedures_controller.rb
@@ -2,14 +2,57 @@
 
 module Api
   module V1
-    class RecipeProceduresController < ApplicationController
-      def index; end
+    class RecipeProceduresController < SecuredController
+      before_action :authorize_request
+      before_action :set_current_user_recipe
+      before_action :set_recipe_procedure, only: %i[update destroy]
 
-      def create; end
+      def index
+        recipe_procedures = @recipe.recipe_procedures.oldest
+        render json: { recipe_procedures: recipe_procedures.map(&:as_json) }, status: :ok
+      end
 
-      def update; end
+      def create
+        # 変数recipe_proceduresとerrorsを複数代入で同時に定義
+        recipe_procedures, errors = RecipeProcedure.build_and_save_all(@recipe, recipe_procedure_params[:procedure])
 
-      def destroy; end
+        if recipe_procedures
+          render json: { recipe_procedures: recipe_procedures }, status: :created
+        else
+          render json: { errors: errors }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @recipe_procedure.update(recipe_procedure_update_params)
+          render json: { recipe_procedure: @recipe_procedure }, status: :ok
+        else
+          render json: { errors: @recipe_procedure.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @recipe_procedure.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_current_user_recipe
+        @recipe = current_user.recipes.find(params[:recipe_id])
+      end
+
+      def set_recipe_procedure
+        @recipe_procedure = @recipe.recipe_procedures.find(params[:id])
+      end
+
+      def recipe_procedure_params
+        params.require(:recipe_procedure).permit(procedure: [])
+      end
+
+      def recipe_procedure_update_params
+        params.require(:recipe_procedure).permit(:procedure)
+      end
     end
   end
 end
--- backend/app/models/recipe_procedure.rb
+++ backend/app/models/recipe_procedure.rb
@@ -4,4 +4,20 @@ class RecipeProcedure < ApplicationRecord
   belongs_to :recipe
 
   validates :procedure, presence: true, length: { maximum: 200 }
+
+  scope :oldest, -> { order(created_at: :asc) }
+
+  # コントローラに渡す
+  def self.build_and_save_all(recipe, procedure_params)
+    recipe_procedures = procedure_params.map do |procedure|
+      recipe.recipe_procedures.build(procedure: procedure)
+    end
+
+    if recipe_procedures.all?(&:save)
+      [ recipe_procedures, nil ]
+    else
+      failed = recipe_procedures.find { |procedure| !procedure.valid? }
+      [ nil, failed.errors.full_messages ]
+    end
+  end
 end
