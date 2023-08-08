3e77e10ae710bb7398158ed4ab6ccc9708379260
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-08-09 02:15:09 +0900

    仕入れ先と原材料取得の際のN+1問題を解消

--- backend/app/models/supplier.rb
+++ backend/app/models/supplier.rb
@@ -8,19 +8,29 @@ class Supplier < ApplicationRecord
   validates :contact_info, length: { maximum: 100 }
 
   scope :oldest, -> { order(created_at: :asc) }
+  scope :with_order_ingredients, -> { includes(:ingredients).order(created_at: :asc) }
 
   # 仕入れ先一覧取得する1つの仕入れ先でページネーションを行う
   def self.with_ingredients_for_user(user, page = 1, per_page = 1)
-    paginated_suppliers = user.suppliers.oldest.select(:id, :user_id, :name,
-                                                       :contact_info).includes(:ingredients).page(page).per(per_page)
+    paginated_suppliers = user.suppliers.with_order_ingredients.page(page).per(per_page)
     suppliers_data = paginated_suppliers.map do |supplier|
+      ingredients_data = supplier.ingredients.map do |ingredient|
+        {
+          id: ingredient.id,
+          supplier_id: ingredient.supplier_id,
+          buy_cost: ingredient.buy_cost,
+          buy_quantity: ingredient.buy_quantity,
+          unit: ingredient.unit,
+          name: ingredient.name
+        }
+      end
+
       {
         id: supplier.id,
         user_id: supplier.user_id,
         name: supplier.name,
         contact_info: supplier.contact_info,
-        ingredients: supplier.ingredients.select(:id, :supplier_id,
-                                                 :buy_cost, :buy_quantity, :unit, :name)
+        ingredients: ingredients_data
       }
     end
     { suppliers: suppliers_data, total_pages: paginated_suppliers.total_pages }
@@ -28,9 +38,13 @@ class Supplier < ApplicationRecord
 
   # 全ての仕入れ先の原材料を取得する
   def self.with_all_ingredients_for_user(user)
-    user.suppliers.oldest.select(:id, :user_id, :name, :contact_info).includes(:ingredients).map do |supplier|
+    suppliers_with_ingredients = user.suppliers.with_order_ingredients
+
+    suppliers_with_ingredients.map do |supplier|
       supplier.attributes.merge(
-        ingredients: supplier.ingredients.select(:id, :supplier_id, :buy_cost, :buy_quantity, :unit, :name)
+        ingredients: supplier.ingredients.map do |ingredient|
+                       ingredient.slice(:id, :supplier_id, :buy_cost, :buy_quantity, :unit, :name)
+                     end
       )
     end
   end
