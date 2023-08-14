070487c341f62cb2ee4a1fdc4fa7e2b8b6f54320
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-08-14 14:04:23 +0900

    タグのkeyがフラグメント使っていた原因で警告が出ていたのを解消とuserの要望で画像を選択していなくても初期値としてno_imageが選択されている状態にしました

--- frontend/components/molecules/checkbox/TagCheckBox.tsx
+++ frontend/components/molecules/checkbox/TagCheckBox.tsx
@@ -66,37 +66,35 @@ export const TagCheckBox = ({ onTagCheckChange }: Props) => {
       <div className="grid grid-cols-3 gap-1 lg:gap-x-4 overflow-y-auto px-3 py-1 h-40 border-2 border-sky-300 rounded-md">
         {tags.length === 0 && !isEditPage && <TagEnptyStates />}
         {tags.map((tag) => (
-          <>
-            <div className="relative flex items-start" key={tag.id}>
-              <div className="flex h-6 items-center">
-                <input
-                  type="checkbox"
-                  aria-describedby="tag-description"
-                  id={tag.id.toString()}
-                  name={tag.name}
-                  className="h-4 w-4 rounded border-gray-300 text-sky-400 focus:ring-sky-400"
-                  checked={
-                    isEditPage
-                      ? checkedEditTags[tag.id] || false
-                      : checkedTags[tag.id] || false
-                  }
-                  onChange={(e) =>
-                    isEditPage
-                      ? handleCheckChangeEdit(tag.id, e.target.checked)
-                      : handleCheckChange(tag.id, e.target.checked)
-                  }
-                />
-              </div>
-              <div className="ml-3 text-sm leading-6">
-                <label
-                  htmlFor={tag.id.toString()}
-                  className="font-bold text-gray-900"
-                >
-                  {tag.name}
-                </label>
-              </div>
+          <div className="relative flex items-start" key={tag.id}>
+            <div className="flex h-6 items-center">
+              <input
+                type="checkbox"
+                aria-describedby="tag-description"
+                id={tag.id.toString()}
+                name={tag.name}
+                className="h-4 w-4 rounded border-gray-300 text-sky-400 focus:ring-sky-400"
+                checked={
+                  isEditPage
+                    ? checkedEditTags[tag.id] || false
+                    : checkedTags[tag.id] || false
+                }
+                onChange={(e) =>
+                  isEditPage
+                    ? handleCheckChangeEdit(tag.id, e.target.checked)
+                    : handleCheckChange(tag.id, e.target.checked)
+                }
+              />
             </div>
-          </>
+            <div className="ml-3 text-sm leading-6">
+              <label
+                htmlFor={tag.id.toString()}
+                className="font-bold text-gray-900"
+              >
+                {tag.name}
+              </label>
+            </div>
+          </div>
         ))}
       </div>
     </fieldset>
