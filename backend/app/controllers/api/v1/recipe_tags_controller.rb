a46c08a5ba060719db1249081bea381245866322
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-08-16 00:51:57 +0900

    不要コード削除 recipe_tagはFormObjectでまとめて登録しているためrecipe#createで解決するのでRecipeTagsControllerは不要認証だけ必要でモデルも必要

--- backend/app/controllers/api/v1/recipe_tags_controller.rb
+++ backend/app/controllers/api/v1/recipe_tags_controller.rb
@@ -4,38 +4,6 @@ module Api
   module V1
     class RecipeTagsController < SecuredController
       before_action :authorize_request
-
-      def index
-        recipe_tags = RecipeTag.where(recipe_id: params[:recipe_id]).includes(:tag)
-        recipe_tags_with_names = recipe_tags.map do |recipe_tag|
-          recipe_tag.attributes.merge({ tag_name: recipe_tag.tag.name })
-        end
-        render json: { recipe_tags: recipe_tags_with_names }, status: :ok
-      end
-
-      # レシピ登録後でもタグのみをチェック入れたら(複数可)特定のレシピからタグを追加できるようにする
-      def create
-        checked_tags = recipe_tag_params_true
-        RecipeTag.add_tags!(params[:recipe_id], checked_tags)
-        render json: { checked_tags: }, status: :created
-      rescue ActiveRecord::RecordInvalid => e
-        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
-      end
-
-      # レシピ登録後でもタグのチェックを外した時に特定のレシピからタグを一つずつ削除する
-      def destroy
-        RecipeTag.delete_tag!(params[:recipe_id], params[:id])
-        head :no_content
-      rescue ActiveRecord::RecordNotFound => e
-        render json: { errors: e.message }, status: :not_found
-      end
-
-      private
-
-      # nextから送られてきたchecked_tagsの中からtrueのものだけを取り出す
-      def recipe_tag_params_true
-        params.require(:checked_tags).permit!.to_h.select { |_k, v| v == true }.keys
-      end
     end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
       get 'search', to: 'search#index'
       resources :recipes, only: %i[index show create update destroy] do
         resources :recipe_ingredients, only: %i[index create update], path: 'ingredients'
-        resources :recipe_tags, only: %i[index create destroy], path: 'tags'
         resources :recipe_procedures, only: %i[index create update destroy], path: 'procedures'
         resource :selling_prices, only: %i[show create update]
       end
