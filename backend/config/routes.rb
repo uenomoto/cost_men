8990fa3dfd2a7cbf7497fb56a43025f19b97afce
上野元希 <uenomotoki@uenomotomarenoMacBook-Air.local>
2023-08-16 01:18:48 +0900

    recipe_ingredientもFormOjectでまとめてAPI叩いているのでコントローラは需要

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
       get 'health_check', to: 'health_check#index'
       get 'search', to: 'search#index'
       resources :recipes, only: %i[index show create update destroy] do
-        resources :recipe_ingredients, only: %i[index create update], path: 'ingredients'
         resources :recipe_procedures, only: %i[index create update destroy], path: 'procedures'
         resource :selling_prices, only: %i[show create update]
       end
